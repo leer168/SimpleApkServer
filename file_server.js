942ab7a0475afaebd89c9d0d2f453a456ea0b5ef
leer168 <leer168@126.com>
2016-08-04 00:11:55 +0800

    Support param dirname

--- file_server.js
+++ file_server.js
@@ -7,7 +7,21 @@ app.use(connect.logger('dev'));
 // respond to all requests
 app.use('/post', function(req, res) {
   console.log("file", req.files.userfile.name);
-  fs.renameSync(req.files.userfile.path, __dirname +'/file/' + req.files.userfile.name);
+  console.log("req.body.dirname", req.body.dirname);
+  var full_path = __dirname +'/file/' + req.body.dirname + "/";
+  console.log("full_path", full_path);
+  fs.exists(full_path, function(is_exist){
+    if(is_exist){
+       fs.renameSync(req.files.userfile.path, full_path + req.files.userfile.name);
+     } else {
+      fs.mkdir(full_path, function(err) {
+        if(!err)
+          fs.renameSync(req.files.userfile.path, full_path + req.files.userfile.name);
+      });
+     }
+
+  });
+ 
   //console.log("body", req.body);
    // don't forget to delete all req.files when done
 });
